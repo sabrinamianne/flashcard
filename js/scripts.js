271fa6f4e2a4d855fe74e3576da2d9e55e423dc8
sabrina mianne <sabrina.mianne@gmail.com>
2019-03-26 16:47:22 -0700

    Added definition, functions click and toggle

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+  $(".clickable").click(function(){
+    $("#text-showing").toggle();
+    $("#text-hidden").toggle();
+});
+
+  $(".clickable1").click(function(){
+    $("#text-showing1").toggle();
+    $("#text-hidden1").toggle();
+  });
+
+  $(".clickable2").click(function(){
+    $("#text-showing2").toggle();
+    $("#text-hidden2").toggle();
+  });
+
+  $(".clickable3").click(function(){
+    $("#text-showing3").toggle();
+    $("#text-hidden3").toggle();
+  });
+
+  $(".clickable4").click(function(){
+    $("#text-showing4").toggle();
+    $("#text-hidden4").toggle();
+  });
+
+  $(".clickable5").click(function(){
+    $("#text-showing5").toggle();
+    $("#text-hidden5").toggle();
+  });
+
+  $(".clickable6").click(function(){
+    $("#text-showing6").toggle();
+    $("#text-hidden6").toggle();
+  });
+
+  $(".clickable7").click(function(){
+    $("#text-showing7").toggle();
+    $("#text-hidden7").toggle();
+  });
+
+  $(".clickable8").click(function(){
+    $("#text-showing8").toggle();
+    $("#text-hidden8").toggle();
+  });
+
+  $(".clickable9").click(function(){
+    $("#text-showing9").toggle();
+    $("#text-hidden9").toggle();
+  });
+  $(".clickable10").click(function(){
+    $("#text-showing10").toggle();
+    $("#text-hidden10").toggle();
+  });
+
+  $(".clickable11").click(function(){
+    $("#text-showing11").toggle();
+    $("#text-hidden11").toggle();
+  });
+
+});
